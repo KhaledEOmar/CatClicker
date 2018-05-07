c0913e5f010867743b171f2a92b46678df4f7e23
Khaled Omar <khaledeomar@gmail.com>
2018-05-07 00:31:09 -0400

    feat:second cat

--- static/js/js.js
+++ static/js/js.js
@@ -1,11 +1,23 @@
 (function($){
 
 	var $counter = 0;
+	var $counter2 = 0;
+	var $cat1name = "Herbert";
+	var $cat2name = "Johnny";
+	
+	$("#cat1name").html($cat1name);
+	$("#cat2name").html($cat2name);
+
 	
 	$('#cat').click(function(e) {
-	  $counter += 1;
-	  $("#counter").html("Click Count: " + $counter);
+		$counter += 1;
+		$("#counter").html("Click Count: " + $counter);
 	});
 
-
+	$('#cat2').click(function(e) {
+		$counter2 += 1;
+		$("#counter2").html("Click Count: " + $counter2);
+	});
+	
+	
 })(jQuery); // end of jQuery name space
